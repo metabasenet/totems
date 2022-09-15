75d1fff7d1f1548e0c88c4c2c90ddade724134bc
flybird000 <flybird000@outlook.com>
2022-09-15 10:44:45 +0800

    modify target

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ module.exports = {
         proxy:{
             "/api":{
                 //target:"http://119.8.55.78:7711/",
-                target:"https://browser.metabasenet.site/api/",
-                //target:"http://127.0.0.1:7711/",
+                //target:"https://browser.metabasenet.site/api/",
+                target:"http://127.0.0.1:7711/",
                 changeOrigin:true,
                 secure: false,
                 ws:true,
